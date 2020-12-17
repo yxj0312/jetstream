81db3c50046129c54a2f1874003dc554b84f1153
yxj0312 <yanxiajun@hotmail.com>
2020-12-17 20:36:27 +0100

    Remove alpinejs from package.json

--- resources/views/alpine/test1.blade.php
+++ resources/views/alpine/test1.blade.php
@@ -5,16 +5,18 @@
         </h2>
     </x-slot>
     
-    <div class="px-10 flex items-center justify-center min-h-screen">
+    <div x-data="{ message: 'Click Me'}" class="px-10 flex items-center justify-center min-h-screen">
         <div class="flex-1 grid grid-cols-4 gap-10">
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
-            <div class="h-32 bg-gray-300">FOO</div>
+            <div class="h-32 bg-gray-300">
+                <button @click="alert('hi there')" x-text="message"></button>
+            </div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
         </div>
     </div>
 </x-app-layout>
