8dde391886ef752b3d29f4e08ceb6d3a7c7437fe
yxj0312 <yanxiajun@hotmail.com>
2020-12-21 22:58:56 +0100

    Ep 4 finished

--- resources/views/alpine/test4.blade.php
+++ resources/views/alpine/test4.blade.php
@@ -6,7 +6,27 @@
     </x-slot>
 
     <div class="p-10 max-w-lg mx-auto">
-        <form action="">
+        <form action=""
+            x-data="{
+                form: {
+                    name: ''
+                },
+
+                user: null,
+
+                submit() {
+                    fetch('https://reqres.in/api/users', {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json'},
+                        body:  JSON.stringify(this.form)
+                    })
+                    .then(response => response.json())
+                    .then(user => this.user = user);
+                }
+            }"
+
+            @submit.prevent="submit"
+        >
             <div class="mb-6">
                 <label class="block mb-2 uppercase font-bold text-xs text-grey-700"
                     for="name"
@@ -14,18 +34,19 @@
                     Name
                 </label>
 
+                <!-- x-model is shorthand of :value="name" @input="name = $event.target.value" -->
                 <input 
                     class="border border-gray-400 p-2 w-full"
                     type="text"
                     name="name"   
                     id="name"
+                    x-model="name"
                     required
                 >
-
             </div>
-
-
+            <template x-if="user">
+                <div x-text="'The user ' + user.name + ' was created at ' + user.createdAt"></div>
+            </template>
         </form>
-
     </div>
 </x-app-layout>
