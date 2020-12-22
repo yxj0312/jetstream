2e6a94aec255cc5f59b2613735f486330214b94c
yxj0312 <yanxiajun@hotmail.com>
2020-12-22 23:02:18 +0100

    ep5 04:33

--- resources/views/alpine/test5.blade.php
+++ resources/views/alpine/test5.blade.php
@@ -6,8 +6,23 @@
     </x-slot>
 
     <div class="p-10 max-w-lg mx-auto">
-        <div class="bg-gray-300 px-10 py-6 rounded">
+        <div class="bg-gray-300 px-10 py-6 rounded" x-data="{ tasks:[], newTask: '' }">
+            <form action="" @submit.prevent="tasks.push({body: newTask, completed: false}); newTask=''">
+                <input 
+                type="text" 
+                placeholder="Go to the market..." 
+                x-model="newTask" 
+                class="w-full px-1">
+            </form>
 
+            <ul class="list-disc mt-3">
+                <template x-for="(task, index) in tasks" :key="index">
+                    <li>
+                        <input type="checkbox" x-model="task.completed">
+                        <span x-text="task.body" :class="{ 'line-through': task.completed }"></span>
+                    </li>
+                </template>
+            </ul>
         </div>
     </div>
 
