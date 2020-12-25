dd76687fa3e95c93fd946859b2ad44c2811ae7ab
yxj0312 <yanxiajun@hotmail.com>
2020-12-25 21:57:40 +0100

    Ep 8 Handling Custom Events finished

--- resources/views/alpine/test8.blade.php
+++ resources/views/alpine/test8.blade.php
@@ -6,8 +6,34 @@
     </x-slot>
 
     <div class="p-12">
-        <div x-data>
-            <button @click="alert('I was clicked')">Click Me</button>
+        <div
+            x-data
+        >
+            <button @click="flash('Hello There')">Trigger Flash Message</button>
+            <!-- with this we can write custom event instead of in the  global script -->
+            <button @click="$dispatch('flash', 'Hello There Again')">Trigger Flash Message</button>
+            <!-- <button @click="$dispatch('flash', { message: 'Foobar', level: 2, color:})">Trigger Flash Message</button> -->
         </div>
+
+        <hr>
+
+        <div 
+            x-data="{ show: false, message: ''}"
+            x-show.transition.opacity.scale.75.duration.3000ms="show"
+            @flash.window="
+            show = true; 
+            message=$event.detail
+            setTimeout(() => show = false, 3000);
+            "
+            x-text="message"
+            class="fixed bottom-0 right-0 mb-4 mr-4 bg-blue-500 text-white p-4 rounded"
+        >
+            
+        </div>
+
+        <script>
+            // call it in console with flash('message');
+            window.flash = message => window.dispatchEvent(new CustomEvent('flash', { detail: message }));
+        </script>
     </div>
 </x-app-layout>
